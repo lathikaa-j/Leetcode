bcfc35f17bbd770186d40d6f81f37d7de13e6638
Lathikaa J <130064380+lathikaa-j@users.noreply.github.com>
2026-07-09 22:06:35 +0530

    Determine whether pairs of nodes belong to the same connected component in a graph formed using a maximum value difference constraint
    Initialize component array and populate based on maxDiff condition.

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
         int[] component = new int[n];
         component[0] = 0;
+        
         for (int i = 1; i < n; i++) {
             if (nums[i] - nums[i - 1] <= maxDiff) {
                 component[i] = component[i - 1];
@@ -9,7 +10,9 @@ class Solution {
                 component[i] = component[i - 1] + 1;
             }
         }
+        
         boolean[] ans = new boolean[queries.length];
+        
         for (int i = 0; i < queries.length; i++) {
             int u = queries[i][0];
             int v = queries[i][1];
